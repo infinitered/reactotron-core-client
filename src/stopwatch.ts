85d3708b3347785c498e473881d8dc0e09628e1b
Richard Evans <rmevans9@gmail.com>
2019-09-23 20:50:47 -0400

    chore: All linting fixed

--- src/stopwatch.ts
+++ src/stopwatch.ts
@@ -10,6 +10,7 @@ const hasHirezNodeTimer =
   typeof process.hrtime === "function"
 
 // the default timer
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const defaultPerformanceNow = (started?: number) => Date.now()
 
 // try to find the browser-based performance timer
